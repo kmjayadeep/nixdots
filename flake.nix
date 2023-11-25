a5133eba9e60c3120b690fdfb4167750249eb4a0
Jayadeep KM <kmjayadeep@gmail.com>
2023-11-25 15:41:45 +0100

    fix syntax
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
     };
 
     # All outputs for the system (configs)
-    outputs = { home-manager, nixpkgs, nur, ... }@inputs: 
+    outputs = { home-manager, nixpkgs, hyprland, ... }@inputs: 
         let
             system = "x86_64-linux"; #current system
             pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
