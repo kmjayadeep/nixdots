cd98b75a11d56eaf35aebefbc2ede84623895352
Jayadeep KM <kmjayadeep@gmail.com>
2023-12-03 13:57:25 +0100

    gtk theme
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- home/theme.nix
+++ home/theme.nix
@@ -6,8 +6,25 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Materia-dark";
-      package = pkgs.materia-theme;
+      name = "Flat-Remix-Darkest-GTK-Blue";
+      package = pkgs.flat-remix-gtk;
+    };
+
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+      gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+      gtk-application-prefer-dark-theme=1
+      '';
     };
   };
 }
