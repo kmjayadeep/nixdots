c6cf3fcd5d483df369f71835a05a517444348118
Jayadeep KM <kmjayadeep@gmail.com>
2024-02-25 16:25:35 +0100

    switch to i3
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- modules/i3.nix
+++ modules/i3.nix
@@ -5,16 +5,15 @@
   ...
 }: {
   services.xserver.enable = true;
-  services.xserver.autorun = false;
   services.xserver.layout = "us";
-  services.xserver.desktopManager.default = "none";
   services.xserver.desktopManager.xterm.enable = false;
   services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.defaultSession = "none+i3";
   services.xserver.windowManager.i3.enable = true;
   
   environment.systemPackages = with pkgs; [
     arandr
-    xrandr # manage monitor
+    xorg.xrandr # manage monitor
     lxappearance
   ];
 
