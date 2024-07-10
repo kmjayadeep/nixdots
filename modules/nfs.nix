50eeacd086a5953a51e3c62cd1df872467dc534d
Jayadeep KM <kmjayadeep@gmail.com>
2024-07-10 15:20:20 +0200

    add documents nfs path
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -22,6 +22,14 @@
       what = "truenas.cosmos.cboxlab.com:/mnt/main/drive";
       where = "/nfs/drive";
     }
+    {
+      type = "nfs";
+      mountConfig = {
+        Options = "noatime";
+      };
+      what = "truenas.cosmos.cboxlab.com:/mnt/main/documents";
+      where = "/nfs/documents";
+    }
   ];
 
   systemd.automounts = [
@@ -38,6 +46,13 @@
         TimeoutIdleSec = "600";
       };
       where = "/nfs/drive";
+    }
+    {
+      wantedBy = ["multi-user.target"];
+      automountConfig = {
+        TimeoutIdleSec = "600";
+      };
+      where = "/nfs/documents";
     }
   ];
 }
