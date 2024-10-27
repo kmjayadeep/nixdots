9aff113dedb58ca158b5db731235444f4b473c77
Jayadeep KM <kmjayadeep@gmail.com>
2024-10-27 18:12:06 +0100

    enable hibernate
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- hosts/jd-pc/hardware-configuration.nix
+++ hosts/jd-pc/hardware-configuration.nix
@@ -24,7 +24,9 @@
       options = [ "fmask=0022" "dmask=0022" ];
     };
 
-  swapDevices = [ ];
+  swapDevices = [
+    {device = "/dev/disk/by-uuid/313cdf0e-f76d-4d30-8c4f-ef6f083837c0";}
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
