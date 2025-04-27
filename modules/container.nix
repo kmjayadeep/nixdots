132747ade6ede61e53bda9aa78f000661f1c0fb4
Jayadeep KM <kmjayadeep@gmail.com>
2025-04-27 20:38:37 +0200

    ipv6 docker
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- modules/container.nix
+++ modules/container.nix
@@ -7,5 +7,10 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = false;
+    autoPrune.enable = true;
+    daemon.settings = {
+      ipv6 = true;
+      fixed-cidr-v6 = "fd82:852b:c100:100::/64";
+    };
   };
 }
