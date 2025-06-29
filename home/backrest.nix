66ec4da1a8dd180ddadc0914d51f53c217070aac
Jayadeep KM <kmjayadeep@gmail.com>
2025-06-29 12:32:08 +0200

    fix paths for backrest so that i can use curl to reach uptime kuma
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- home/backrest.nix
+++ home/backrest.nix
@@ -17,8 +17,11 @@
         "BACKREST_RESTIC_COMMAND=${pkgs.restic}/bin/restic"
         "BACKREST_CONFIG=${config.home.homeDirectory}/private/backrest/config"
         "BACKREST_DATA=${config.home.homeDirectory}/private/backrest/data"
+        "PATH=${pkgs.lib.makeBinPath [ pkgs.bash pkgs.curl pkgs.coreutils pkgs.restic ]}"
       ];
       ExecStart = "${pkgs.backrest}/bin/backrest";
+      Restart = "on-failure";
+      RestartSec = "5";
     };
 
     Install = {
