f98c6e9f165b0e7e457c3fb94f69f63f640ed4bf
Jayadeep KM <kmjayadeep@gmail.com>
2026-01-29 18:39:43 +0100

    fix fold
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- home/nvim/plugin/treesitter.lua
+++ home/nvim/plugin/treesitter.lua
@@ -10,6 +10,7 @@ vim.api.nvim_create_autocmd('FileType', {
     -- Folding
     vim.wo[0][0].foldmethod = 'expr'
     vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldlevel = 99  -- Open all folds by default
 
     -- Indentation (experimental)
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
